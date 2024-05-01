8b7cf26d9abbe999f45d5a5cf49c039052c37f8e
lizwachira <lizwanguwachira@gmail.com>
2024-05-01 13:37:01 +0300

    final commit

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -9,7 +9,12 @@ function ExampleComponent() {
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
+      <img
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
+      />
     </div>
+    
   );
 }
 
